ab2bf8fc1f313d2072195ae106f854683b3d1b89
Luciano Pinheiro <luciano.pinheiro@gmail.com>
2021-05-27 01:06:00 -0300

    Segmenting and Clustering Neighborhoods in Toronto

---
+++ foursquare.py
@@ -0,0 +1,13 @@
+# notes - please ignore
+
+url_venues = 'https://api.foursquare.com/v2/venues'
+# premium https://api.foursquare.com/v2/venues/121fd2f1ds2f12f1c2v1?client_id=*****&client_secret=****&v=20180602
+
+url_user = 'https://api.foursquare.com/v2/users'
+# regular https://api.foursquare.com/v2/users/123456?client_id=*****&client_secret=****&v=20180602
+
+url_venues_explore = 'https://api.foursquare.com/v2/venues/explore'
+# premium 
+
+url_venues_trending = 'https://api.foursquare.com/v2/venues/trending'
+
